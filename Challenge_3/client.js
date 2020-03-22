28f912597e947724faae371e105e7c637016595e
Dylan Seery <32263323+Dylanseery17@users.noreply.github.com>
2020-03-22 16:32:53 +0000

    Commented Challenge 3

--- Challenge_3/client.js
+++ Challenge_3/client.js
@@ -1,29 +1,37 @@
+// Node js libary allowing RPC Remote procedure call
 var hprose = require("hprose");
+// Proxy to talk to server on port 8080
 var client = hprose.Client.create("http://127.0.0.1:8080/");
 var proxy = client.useService(["xmlParse"]);
 // Node js libary allowing to convert to XMLHttpRequest
 const DOMParser = require('xmldom').DOMParser;
 const fs = require('fs');
 
+// Express.js for routing client side application
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', async (req, res) => {
     try {
+        // File read in and setup
         let data = fs.readFileSync('data.xml', 'utf8').toString();
-        data =  await change_Command(data.toString())
+        // Altering XML FILE Can be commented Test functions
+        data =  await change_Command(data.toString());
         data = await change_Site_id(data.toString());
         const result = await proxy.xmlParse(data);
+        // Displaying the result on webpage
         res.send(result);
     } catch (error) {
         res.send("Error sending file to server");
     }
 });
 
+// Testing changing the command from DEFAULT 
 const change_Command = (xml) => {
     let errors = [];
 
+    // Reading in doc overriding error handler to get errors
     let doc  = new DOMParser({
         errorHandler: {
            warning: (msg) => {errors.push(msg)},
@@ -33,6 +41,8 @@ const change_Command = (xml) => {
     }).parseFromString(xml);
 
     if(errors.length > 0) return errors.toString();
+
+    // Changing the Declaration Command From DEFAULT To NOT_DEFAULT
     let declaration  = doc.getElementsByTagName("Declaration");
     declaration[declaration.length-1].attributes.getNamedItem("Command").value = "NOT_DEFAULT";
 
@@ -43,6 +53,7 @@ const change_Command = (xml) => {
 const change_Site_id = (xml) => {
     let errors = [];
 
+    // Reading in doc overriding error handler to get errors
     let doc  = new DOMParser({
         errorHandler: {
            warning: (msg) => {errors.push(msg)},
@@ -53,6 +64,7 @@ const change_Site_id = (xml) => {
 
     if(errors.length > 0) return errors.toString();
 
+    // Changing the SiteID From DUB To LDN
     let site_id = doc.getElementsByTagName("SiteID")
     site_id[0].childNodes[0].data = "LDN";
 
--- Challenge_3/server.js
+++ Challenge_3/server.js
@@ -1,3 +1,4 @@
+// Node js libary allowing RPC Remote procedure call
 var hprose = require("hprose");
 // Node js libary allowing to convert to XMLHttpRequest
 const DOMParser = require('xmldom').DOMParser;
@@ -5,6 +6,8 @@ const DOMParser = require('xmldom').DOMParser;
 function xmlParse(xml_data){
     let val, doc, declaration, command, site_id;
     let errors = [];
+
+    // Reading in doc overriding error handler to get errors
     doc  = new DOMParser({
         errorHandler: {
            warning: (msg) => {errors.push(msg)},
@@ -16,19 +19,23 @@ function xmlParse(xml_data){
     if(errors.length == 0) val = 0;
     else return errors;
 
+    // Retriving the Declaration, Command
     declaration  = doc.getElementsByTagName("Declaration");
     command  = declaration[declaration.length-1].attributes.getNamedItem("Command").value;
+    // Site_id the Declaration, Command
     site_id  = doc.getElementsByTagName("SiteID")[0].childNodes[0].nodeValue;
     
+    // Checks for DEFAULT AND DUB
     if(command == "DEFAULT") val = -1;
     if(site_id == "DUB") val = -2;
     
+    // RETURN based on (VAL) value
     if(val == 0) return [val + ' - correct structure.']; 
     else if(val == -1) return [val + ' - invalid command specified.'];
     else if(val == -2) return [val + ' - invalid Site specified.'];
 }
 
-
+// Creating the server and adding the function to the server
 var server = hprose.Server.create("http://127.0.0.1:8080");
 server.addFunction(xmlParse);
 server.start();
